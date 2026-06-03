6618934d0341f1eb9656dd1ec1f330c51cb98701
zimbermann <121502209+zimbermann@users.noreply.github.com>
2026-06-03 12:23:02 +0300

    again

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    allowedHosts: ["all"],
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT || "4173"),
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
