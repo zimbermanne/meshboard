02fc7f937e36479f57e2c27595ba986794928ac5
zimbermanne <zimbermannelit@gmail.com>
2026-06-03 09:40:34 +0300

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,8 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     allowedHosts: true,
+    host: "0.0.0.0",
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
   },
   server: {
     port: 3000,
