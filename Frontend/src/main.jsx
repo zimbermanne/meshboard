6415ab96648131fc339eb451b24a20e26f3ad67c
zimbermann <121502209+zimbermann@users.noreply.github.com>
2026-06-06 12:01:09 +0300

    updates

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Remove or comment out this line if you don't have an index.css file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
